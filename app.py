3fc3fdade58915d8e5a0c19e0917dce6024299d6
Jamie Stephens <71529189+jstephens@users.noreply.github.com>
2021-07-21 05:15:35 -0400

    changes

--- app.py
+++ app.py
@@ -13,15 +13,53 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 from sqlalchemy import create_engine
+import numpy as np
+import geojson
+import plotly.graph_objs as go
 
-engine = create_engine('sqlite:///forex.db'.connect()
-                       
-                       
-def makeheatmapdf:
-    df = pd.read_sql_table('employee', engine)
-    
-    
+engine = create_engine('sqlite:///forex.db')
 
+currencies = ['JPY','GBP','EUR','AUD','NZD','CNY','HKD','SGD','INR','MXN','PHP','THB','MYR','ZAR','RUB']
+
+def makeheatmapdf():
+    df = pd.read_sql_table('heatmaptable', engine)
+    df
+    
+    final_df = pd.DataFrame(columns=['Currency','First','Last'])
+    for i in currencies:
+        if i in df.columns:
+            first_value = df[i].loc[~df[i].isnull()].iloc[0]
+            last_value = df[i].loc[~df[i].isnull()].iloc[-1]
+            final_df.loc[len(final_df.index)] = [i, first_value,last_value]
+    final_df['Difference'] = final_df['Last'] - final_df['First']
+    final_df['Percent Chg'] = final_df['Difference'] / final_df['First']
+    final_df.reset_index()
+    country_convert = {'JPY':'JPN',
+                       'GBP':'GBR',
+                       'EUR':'DEU',
+                       'AUD':'AUS',
+                       'NZD':'NZL',
+                       'CNY':'CHN',
+                       'HKD':'HKG',
+                       'SGD':'SGP',
+                       'INR':'IDN',
+                       'MXN':'MEX',
+                       'PHP':'PHL',
+                       'THB':'THA',
+                       'MYR':'MYS',
+                       'ZAR':'ZAF',
+                       'RUB':'RUS'
+        }
+    print(final_df)
+    final_df['Currency'] = final_df['Currency'].map(country_convert)
+    print(final_df)
+    fig = px.choropleth(final_df, locations="Currency",
+                    color="Percent Chg",
+                    hover_name="Currency",
+                    color_continuous_scale=px.colors.sequential.Plasma)
+    return fig
+    
+fig = makeheatmapdf()
 
 external_stylesheets = [
     'https://codepen.io/chriddyp/pen/bWLwgP.css',
@@ -46,7 +84,9 @@ app.layout = html.Div(children=[
     ),
     html.Div(children='Jamie Stephens', style={
         'textAlign': 'center'
-    })
+    }),
+    html.Div([
+    dcc.Graph(figure=fig)])
 ])
 
 if __name__ == '__main__':
--- database_creation.py
+++ database_creation.py
@@ -14,7 +14,7 @@ import datetime
 from pytz import all_timezones
 import pytz
 
-currencies = ['JPY=X','GBPUSD=X','AUDUSD=X','NZDUSD=X','CNY=X','HKD=X','SGD=X','INR=X','MXN=X','PHP=X','THB=X','MYR=X','ZAR=X','RUB=X']
+currencies = ['JPY=X','GBPUSD=X','EURUSD=X','AUDUSD=X','NZDUSD=X','CNY=X','HKD=X','SGD=X','INR=X','MXN=X','PHP=X','THB=X','MYR=X','ZAR=X','RUB=X']
 engine = create_engine('sqlite:///forex.db', echo = True)
 meta = MetaData()
 
@@ -32,11 +32,6 @@ for i in currencies:
     df_1 = yf.download(tickers=i,period='7d',interval='1m')
     
     df_1.tz_convert('Europe/London')
-    
-    #local1 = pytz.timezone("Europe/London")
-    #local_dt1 = local1.localize(df_1, is_dst=None)
-    #df_1 = df_1 = df_1.tz_localize(tz = 'Europe / London')
-   # df_1.index = df_1.index.tz_convert(pytz.utc)
     df_1 = df_1.reset_index()
     
     table_name = 'minutetable_' + j
@@ -53,27 +48,15 @@ for i in currencies:
         heat_df = heat_df.set_index('Datetime')
         heat_df = heat_df = heat_df.tz_localize(tz = 'Europe/London')
         
-        #f = datetime.datetime.now().replace(second=00).replace(microsecond=0)
-        #days = datetime.timedelta(7)
-        
-        #local = pytz.timezone("US/Eastern")
-        #local_dt = local.localize(f, is_dst=None)
-        #utc_dt = local_dt.astimezone(pytz.utc)
-        #g = f - days
-        #rng = pd.date_range(g, periods=10080, freq='T')
-        #print(type(rng))
-        #heat_df['Datetime'] = rng
     del df_1['Open']
     del df_1['High']
     del df_1['Low']
+    del df_1['Adj Close']
+    del df_1['Volume']
+    df_1.rename(columns = {'Close':j}, inplace = True)
     heat_df = pd.merge(heat_df,df_1,on='Datetime',how='outer')
-    heat_df.rename(columns={ heat_df.columns[-1]: j }, inplace = True)  
- #   heat_df.set_axis([*heat_df.columns[:-1], j], axis=1, inplace=False)
     
-
-print(heat_df)
-
-heat_df.to_csv('test.csv')
+heat_df.to_sql('heatmaptable',engine)
 
 #driver = webdriver.Firefox()
 #driver.get("http://www.python.org")
